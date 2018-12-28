f3a2bcd5f7553f4eed964b5e8f19f3976c66ac43
Ivan Zinchenko <zinchenko291@gmail.com>
2018-12-28 23:13:32 +0500

    git init

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require("browser-sync");
+
+gulp.task("sass", function() {
+    return gulp.src("./app/sass/**/*.scss",)
+    .pipe(sass({outputStyle:"expanded"}).on("error", sass.logError))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest("./app/css"))
+    .pipe(browserSync.reload({stream: true}))
+})
+
+gulp.task("browser-sync", function() {
+    browserSync({
+        server: {
+            baseDir: "./app"
+        },
+        notify: false
+    })
+})
+
+gulp.task("watch", function() {
+    gulp.watch("./app/sass/*.scss", gulp.series("sass"));
+    gulp.watch("./app/*.html").on("change", browserSync.reload);
+})
+
+gulp.task("default", gulp.parallel("watch" ,"browser-sync"));
